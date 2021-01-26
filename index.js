c4372530cdf4c250f4867b1ad0b368009015dab1
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2021-01-26 23:40:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config()
 const app = require("express")()
-const cors = require("cors")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const jsonParser = bodyParser.json()
@@ -15,11 +14,8 @@ const {
 const PORT = process.env.PORT || 5000
 
 //Middleware
-
-app.use(cors({
-	credentials: true,
-	origin: 'https://beed-client.netlify.app'
-}))
+const cors = require("cors")
+app.use(cors())
 app.use(
 	fileUpload({
 		useTempFiles: true,
@@ -27,11 +23,11 @@ app.use(
 	})
 )
 app.use(jsonParser)
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-  });
+//app.all('/', function(req, res, next) {
+  //  res.header("Access-Control-Allow-Origin", "*");
+  //  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  //  next()
+ // });
   
 //Routes
 app.get("/fetch-auctions", fetchAuctions)
