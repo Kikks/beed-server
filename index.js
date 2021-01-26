6d2b6df4e8d561dd50d40661d2e8265372c3f81f
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2021-01-26 23:10:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const PORT = process.env.PORT || 5000
 
 //Middleware
 
-app.use(cors())
+app.use(cors({
+	origin: 'https://beed-client.netlify.app'
+}))
 app.use(
 	fileUpload({
 		useTempFiles: true,
